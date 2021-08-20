1bfd695b595b7838199e0534bb82f1bf1605bef4
Juan Padilla <martin22padilla@gmail.com>
2021-08-20 09:53:05 -0300

    Commit

---
+++ back/IngresoSML2/Controllers/CustomersController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IngresoSML2.Data;
+using Newtonsoft.Json;
+
+namespace IngresoSML2.Controllers
+{
+    [Route("/api/Customer")]
+    [ApiController]  
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+
+        public CustomersController(AppDbContext context)
+        {
+            dbContext = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            var customer = this.dbContext.Customers.Include(c => c.Invoices).ThenInclude(x => x.Items).ToArray();
+            var list = JsonConvert.SerializeObject(customer, Formatting.None, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            });
+            return Ok(list);
+        }
+
+        [HttpGet("Detail/{Id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (!CustomerExists(id))
+            {
+                return NotFound();
+            }
+            var customer = this.dbContext.Customers.Where(u => u.CustomerId == id).Include(c => c.Invoices).ThenInclude(x => x.Items).FirstOrDefault();
+            var list = JsonConvert.SerializeObject(customer, Formatting.None, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            });
+            return Ok(list);
+        }
+
+
+
+        [HttpPost("Add")]
+
+        public async Task<IActionResult> Add([Bind("CustomerId,FirstName,LastName,Address")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customer.CustomerId = getCustomerId();
+                this.dbContext.Add(customer);
+                this.dbContext.SaveChanges();
+                return RedirectToAction(nameof(Details), new { id = customer.CustomerId });
+
+
+            }
+            return NotFound();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return this.dbContext.Customers.Any(e => e.CustomerId == id);
+        }
+
+
+        private int getCustomerId()
+        {
+            return this.dbContext.Customers.Count() + 1;
+        }
+         
+    }
+}
---
+++ back/IngresoSML2/Controllers/InvoiceController.cs
@@ -0,0 +1,88 @@
+﻿using IngresoSML2.Data;
+using IngresoSML2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IngresoSML2.Controllers
+{
+    [Route("/api/invoice")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+
+        public InvoiceController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add([FromBody] InvoicePostModel pm)
+        {
+            if (ModelState.IsValid)
+            {
+                Invoice invoice = new Invoice
+                {
+                    Id = this.getInvoiceId(),
+                    CustomerId = pm.CustomerId,
+                    Date = pm.Date,
+                };
+                this.dbContext.Invoices.Add(invoice);
+                int id = getInvoiceItemId();
+                foreach (string items in pm.Codes)
+                {
+                    
+                    this.dbContext.InvoiceItems.Add(new InvoiceItem()
+
+                    {
+                        InvoiceId = invoice.Id,
+                        InvoiceItemId = id,
+                        ProductCode = items
+                    });
+                    id++;
+                }
+                this.dbContext.SaveChanges();
+                return RedirectToAction(nameof(Details), new { id = invoice.Id }); ;
+
+            }
+            return NotFound();
+
+        }
+
+        [HttpGet("Details/{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (!InvoiceExists(id))
+            {
+                return NotFound();
+            }
+            var invoce = this.dbContext.Invoices.Where(u => u.Id == id).Include(u => u.Items).FirstOrDefault();
+            var list = JsonConvert.SerializeObject(invoce, Formatting.None, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            });
+            return Ok(list);
+        }
+        private bool InvoiceExists(int id)
+        {
+            return this.dbContext.Invoices.Any(e => e.Id == id);
+        }
+
+        private int getInvoiceId()
+        {
+            return this.dbContext.Invoices.Count() + 1;
+        }
+        private int getInvoiceItemId()
+        {
+            return this.dbContext.InvoiceItems.Count() + 1;
+        }
+
+
+
+    }
+}
